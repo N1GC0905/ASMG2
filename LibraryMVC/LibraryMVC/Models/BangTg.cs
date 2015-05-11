9078f3ebc340aa7cd64c7b181fb69b9e37a2bcd6
Nhom1GC0905 <ctdp95@gmail.com>
2015-05-11 11:19:46 +0700

    no message
    Signed-off-by: vuongnd <vuongndgc00702@fpt.edu.vn>

---
+++ LibraryMVC/LibraryMVC/Models/BangTg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryMVC.Models
+{
+    public class BangTg
+    {
+        public int MaTG { get; set; }
+
+        public string TenTG { get; set; }
+
+        public string DiaChi { get; set; }
+
+        public int SDT { get; set; }
+        public string Email { get; set; }
+    }
+}
