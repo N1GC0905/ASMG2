da02248cba8411d8d2c39073bb22e38f6afc1083
vuongndgc <vuongndgc00702@fpt.edu.vn>
2015-05-13 11:15:52 +0700

    no message

---
+++ LibraryMVC/LibraryMVC/DAI/ContosoContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace LibraryMVC.DAI
+{
+    public class ContosoContext : DbContext
+    {
+        public ContosoContext() : base("ContosoContext")
+        {
+        }
+        
+        public DbSet<LibraryMVC.Models.BangSach> BacngSachs { get; set; }
+        public DbSet<LibraryMVC.Models.BangTG> BangTGs { get; set; }
+        public DbSet<LibraryMVC.Models.BangNXB> BangNXBs { get; set; }
+        public DbSet<LibraryMVC.Models.BangLoaiSach> TheLoai { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
