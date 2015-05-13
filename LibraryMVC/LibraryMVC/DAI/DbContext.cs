b5a6c6526e0edd163e99797286a7593014cba230
vuongndgc <vuongndgc00702@fpt.edu.vn>
2015-05-13 20:24:08 +0700

    no message

--- LibraryMVC/LibraryMVC/DAI/DbContext.cs
+++ LibraryMVC/LibraryMVC/DAI/DbContext.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryMVC.DAI
+{
+    class DbContext
+    {
+        private string p;
+
+        public DbContext(string p)
+        {
+            // TODO: Complete member initialization
+            this.p = p;
+        }
+    }
+}
