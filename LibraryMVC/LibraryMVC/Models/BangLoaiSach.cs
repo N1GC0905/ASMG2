1aadb8166377c598f1962389b7e99fda743b2238
vuongndgc <vuongndgc00702@fpt.edu.vn>
2015-05-13 10:12:10 +0700

    no message

---
+++ LibraryMVC/LibraryMVC/Models/BangLoaiSach.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryMVC.Models
+{
+    public class BangLoaiSach
+    {
+        public int MaLoai { get; set; }
+        public string TenLoai { get; set; }
+    }
+}
