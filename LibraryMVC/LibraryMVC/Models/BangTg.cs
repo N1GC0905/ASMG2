acdef8a32f7a30e52e1061340ae05c11ffb54676
vuongndgc <vuongndgc00702@fpt.edu.vn>
2015-05-13 10:09:58 +0700

    no message

--- LibraryMVC/LibraryMVC/Models/BangTg.cs
+++ LibraryMVC/LibraryMVC/Models/BangTg.cs
@@ -5,14 +5,11 @@ using System.Web;
 
 namespace LibraryMVC.Models
 {
-    public class BangTg
+    public class BangTG
     {
         public int MaTG { get; set; }
-
         public string TenTG { get; set; }
-
         public string DiaChi { get; set; }
-
         public int SDT { get; set; }
         public string Email { get; set; }
     }
