c6e9a5894f4fd340763f02b0b877ceeaf9864f5f
vuongndgc <vuongndgc00702@fpt.edu.vn>
2015-05-13 10:08:07 +0700

    no message

---
+++ LibraryMVC/LibraryMVC/Models/BangNXB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryMVC.Models
+{
+    public class BangNXB
+    {
+        public int MaNXB { get; set; }
+        public string TenNXB { get; set; }
+        public string DiaChi { get; set; }
+        public int SDT { get; set; }
+        public string Email { get; set; }
+    }
+}
